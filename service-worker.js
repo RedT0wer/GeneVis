00b383860750cce0e30b8562b88d933c7f5f4c9b
RedT0wer <147622032+RedT0wer@users.noreply.github.com>
2026-08-23 16:01:43 +0400

    Forced updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,21 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    }).then(() => self.clients.claim())
   );
 });
 
